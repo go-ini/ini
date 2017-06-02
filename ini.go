d3de07a94d22b4a0972deb4b96d790c2c0ce8333
Unknwon <u@gogs.io>
2017-06-02 16:46:24 -0400

    struct: add strict mode to MapTo series (#96)

--- ini.go
+++ ini.go
@@ -37,7 +37,7 @@ const (
 
 	// Maximum allowed depth when recursively substituing variable names.
 	_DEPTH_VALUES = 99
-	_VERSION      = "1.27.3"
+	_VERSION      = "1.28.0"
 )
 
 // Version returns current package version literal.
