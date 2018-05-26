7af1ffd45d624cf8a055ae35ab74ea007d9377c6
Anton Antonov <anton.synd.antonov@gmail.com>
2018-05-27 01:39:42 +0300

    parser: support ignore inline comment (#146)
    Edge case has a value that contains an inline symbol such as `;` or `#`.
    
    Currently parsing a value such as:
    
    ```
    value = val#ue
    ```
    
    would result into just `val` if `IgnoreInlineComment` is false.
    
    If `IgnoreInlineComment` is true it would result into the full value, but it's incorrect to depend on this.
    
    With the fix value would be parsed as `val#ue`.

--- ini.go
+++ ini.go
@@ -145,6 +145,11 @@ type LoadOptions struct {
 	// Relevant quote:  Values can also span multiple lines, as long as they are indented deeper
 	// than the first line of the value.
 	AllowPythonMultilineValues bool
+	// SpaceBeforeInlineComment indicates whether to allow comment symbols (\# and \;) inside value.
+	// Docs: https://docs.python.org/2/library/configparser.html
+	// Quote: Comments may appear on their own in an otherwise empty line, or may be entered in lines holding values or section names.
+	// In the latter case, they need to be preceded by a whitespace character to be recognized as a comment.
+	SpaceBeforeInlineComment bool
 	// UnescapeValueDoubleQuotes indicates whether to unescape double quotes inside value to regular format
 	// when value is surrounded by double quotes, e.g. key="a \"value\"" => key=a "value"
 	UnescapeValueDoubleQuotes bool
