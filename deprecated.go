151f3d74ebb921655b1944dac191826c9224b0a8
Jade Clarke <jade-clarke@clarkeholdings.ca>
2021-06-21 11:49:59 -0400

    Making DefaultSection modifyable to handle aws profile default.

--- deprecated.go
+++ deprecated.go
@@ -14,12 +14,10 @@
 
 package ini
 
-const (
-	// Deprecated: Use "DefaultSection" instead.
-	DEFAULT_SECTION = DefaultSection
-)
-
 var (
 	// Deprecated: AllCapsUnderscore converts to format ALL_CAPS_UNDERSCORE.
 	AllCapsUnderscore = SnackCase
+
+	// Deprecated: Use "DefaultSection" instead.
+	DEFAULT_SECTION = DefaultSection
 )
